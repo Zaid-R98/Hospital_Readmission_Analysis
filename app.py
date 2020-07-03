f709dd28c7a98b215dbc219416c436bbe6ccd6fd
Zaid Rahman <zaidrahman0@gmail.com>
2020-07-04 01:47:12 +0400

    Working commit

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
 from flask import Flask, request, jsonify, render_template
 from forms import PredictForm
+from tensorflow import keras
+import pandas as pd
+import numpy as np
+import joblib
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisjustatest'
 @app.route('/', methods=['GET','POST'])
 def indexfunc():
     form=PredictForm()
-    first=form.first.data
-    print('the value entered was', first)
-    return render_template('regression.html', form=form)
+    model = keras.models.load_model("price_prediction_model.h5")
+    transformer = joblib.load("data_transformer.joblib")
+    prediction_text=' Not Validated Yet..'
+    if form.validate_on_submit():
+        newdict={
+                'age': [str(form.age.data)],
+                'time_in_hospital': [int(form.time_in_hospital.data)],
+                'num_medications': [int(form.num_medications.data)],
+                'number_diagnoses': [int(form.number_diagnoses.data)],
+                'metformin':[str(form.metformin.data)],
+                'chlorpropamide':[str(form.chlorpropamide.data)],
+                'glimepiride':[str(form.glimepiride.data)],
+                'tolazamide':[str(form.tolazamide.data)],
+                'insulin':[str(form.insulin.data)],
+                'race':[str(form.race.data)],
+                'admission_type_id':[int(form.admission_type_id.data)],
+                'admission_source_id':[int(form.admission_source_id.data)],
+                'max_glu_serum':[str(form.max_glu_serum.data)],
+                'A1Cresult':[str(form.A1Cresult.data)]
+            }
+        newds=pd.DataFrame(newdict)
+
+        prediction = model.predict(transformer.transform(newds))
+
+        max_index_col = np.argmax(prediction, axis=1)
+
+        if max_index_col==0:
+            prediction_text=' The Patient will be readmitted > 30 Times '
+
+        if max_index_col==1:
+            prediction_text=' The Patient will be readmitted < 30 Times '
+
+        if max_index_col==2:
+            prediction_text=' The Patient will be not be readmitted '
+   
+    return render_template('regression.html', form=form,prediction_text=prediction_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -2,25 +2,32 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField,ValidationError,SelectField,IntegerField,TextAreaField
 
 class PredictForm(FlaskForm):
-    first=IntegerField('Enter the Value Please')
-    second=IntegerField('Enter the Value Please')
-    third=IntegerField('Enter the Value Please')
-    fourth=IntegerField('Enter the Value Please')
-    fifth=IntegerField('Enter the Value Please')
-    sixth=IntegerField('Enter the Value Please')
-    seventh=IntegerField('Enter the Value Please')
-    eight=IntegerField('Enter the Value Please')
-    nine=IntegerField('Enter the Value Please')
-    ten=IntegerField('Enter the Value Please')
-    eleven=IntegerField('Enter the Value Please')
-    twelve=IntegerField('Enter the Value Please')
-    thirteen=IntegerField('Enter the Value Please')
-    fourteen=IntegerField('Enter the Value Please')
-    fifteen=IntegerField('Enter the Value Please')
-    sixteen=IntegerField('Enter the Value Please')
-    seventeen=IntegerField('Enter the Value Please')
-    eighteen=IntegerField('Enter the Value Please')
-    nineteen=IntegerField('Enter the Value Please')
-    twenty=IntegerField('Enter the Value Please')
-    submit = SubmitField('Predict!')
+    age=StringField(' Enter the age of the patient')
 
+    time_in_hospital=IntegerField('Enter the time in hospital')
+
+    num_medications=IntegerField('Enter the number of medications')
+
+    number_diagnoses=IntegerField('Enter the number of diagnosis done')
+
+    metformin=StringField('Enter the Metmorphin Level')
+
+    chlorpropamide=StringField('Enter the chlorpropamide level')
+
+    glimepiride=StringField('Enter the glimepiride level')
+
+    tolazamide=StringField('Enter the tolazamide level')
+
+    insulin=StringField('Enter the insulin level')
+
+    race=StringField('Enter the race of the patient')
+
+    admission_type_id=IntegerField('Enter the admission type ID of the patient')
+
+    admission_source_id=IntegerField('Enter the admission source ID')
+
+    max_glu_serum=StringField('Enter the max_glu_serum level')
+
+    A1Cresult=StringField('Enter the A1Cresult level')
+
+    submit=SubmitField('Predict!')
